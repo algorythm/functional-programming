0bc65030566492428383a71a10851556f55df086
algorythm <anders@wiberg.tech>
2018-09-23 19:12:32 +0200

    wk02, ex04.3

---
+++ exercises-fs/wk02/ex4-3.fsx
@@ -0,0 +1,8 @@
+// Book, exercise 4.3, page 89
+let evenN(n) = [2..2..n*2]
+evenN(-10) // Expected []
+evenN(1) // Expected [2]
+evenN(3) // Expected [2, 4, 6]
+evenN(4) // Expected [2; 4; 6; 8]
+evenN(6) // Expected [2; 4; 6; 8; 10; 12]
+evenN(10) // Expected [2; 4; 6; 8; 10; 12; 14; 16; 18; 20]
