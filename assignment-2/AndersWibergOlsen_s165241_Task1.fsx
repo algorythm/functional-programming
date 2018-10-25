91aaa60aeba84f98dd2630635c756abb36ececd8
algorythm <anders@wiberg.tech>
2018-10-25 21:38:21 +0200

    task 2.3 done

--- assignment-2/AndersWibergOlsen_s165241_Task1.fsx
+++ assignment-2/AndersWibergOlsen_s165241_Task1.fsx
@@ -1,4 +1,5 @@
-open System.Security
+let test expected actual = System.Console.WriteLine("expected: {0}\n  actual: {1}", expected, actual)
+
 // ############
 // # Task 2.1 #
 // ############
@@ -14,12 +15,12 @@ let rec isGround = function
 and isGroundTermList = function
     | [] -> true
     | [t] -> isGround t
-    | _::tlist -> isGroundTermList tlist
+    | _ :: tlist -> isGroundTermList tlist
 
 // Expected result: false
 isGround(V("Should return false"));; 
 // Expected result: true
-isGround(C(0));; 
+isGround(C(0))
 // Expected result: false
 isGround(F("Should return false", [C(0); V"Should return false"]))
 // Expected result: true
@@ -40,7 +41,7 @@ let rec toString = function
 and toStringTermList = function
     | [] -> ""
     | [elm] -> toString elm
-    | elm::rest -> toString elm + "," + toStringTermList rest
+    | elm :: rest -> toString elm + "," + toStringTermList rest
 
 // Expected result: f3(f2(1,2),f1(x),f0())
 toString(F("f3",[F("f2",[C 1; C 2]); F("f1",[V "x"]); F("f0",[])]));;
@@ -49,9 +50,31 @@ toString(F("f3",[F("f2",[C 1; C 2]); F("f1",[V "x"]); F("f0",[])]));;
 // # Task 2.3 #
 // ############
 
-// I don't really like the formulation of this task. As I understand it:
+// I don't really like the wording of this task. As I understand it:
 //     when calling:    subst x t' t
 //     then every occurence of t'   in   t  should be replaced by x such that
-//     subst "x" "y" V("x")    return   V("y")
-//     subst 1 2 F("test", [C 1; C 2]) return F("test", [C 2; C 2])
-let rec subst x tm t = function
+//     subst "x" C(0) V("x")    return   C 0
+//     subst "test" C(2) F("test", [C 1; V("test")]) return F("test", [C 1; C 2])
+//     subst "test" C(2) F("beep", [C 1; V("test")]) return F("beep", [C 1; C 2])
+
+let rec subst x tm = function
+    | V v when v = x -> tm 
+    | F (f, flist) -> F(f, substFList x tm flist)
+    | other -> other
+and substFList x tm = function
+    | [] -> []
+    | [elm] -> [subst x tm elm]
+    | elm :: rest -> subst x tm elm :: substFList x tm rest
+
+let t0 = C 0
+let t1 = V "x"
+// Expected result: C 0
+subst "x" t0 t1
+// Expected result: V ("x")
+subst "y" t0 t1
+let t2 = F("test", [C 1; V "test"])
+let t3 = F("test", [C 1; V "beep"])
+// Expected result: F("test", [C 1; C 0])
+subst "test" t0 t2
+// Expected result: F("test", [C 1; V "Beep"])
+subst "test" t0 t3
